f34a4e155deedfcb7a5707a65d102d694c6be208
AndriFP <andrifebriyadiputra@gmail.com>
2023-11-02 12:57:50 +0700

    lanjutan

--- app/App/View.php
+++ app/App/View.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 namespace GroupDuaPBD\Management\Login\Php\App;
-=======
-namespace ProgrammerZamanNow\Belajar\PHP\MVC\App;
->>>>>>> parent of 00c17c2 (Konfigurasi Awal dan Database)
+
 
 class View
 {
